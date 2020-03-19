945e02232fa97bd454c69d2d7f6007e4cf655d35
Mikael Vaaltola <mvaaltola@gmail.com>
2020-03-19 15:39:43 +0200

    Complete 1.2

--- 1/1.2/src/index.js
+++ 1/1.2/src/index.js
@@ -10,6 +10,15 @@ const Header = (props) => {
 }
 const Content = (props) => {
   return(
+    <div>
+      <Part part={props.parts[0]} exercises={props.exercises[0]} />
+      <Part part={props.parts[1]} exercises={props.exercises[1]} />
+      <Part part={props.parts[2]} exercises={props.exercises[2]} />
+    </div>
+  )
+}
+const Part = (props) => {
+  return(
     <p>
       {props.part} {props.exercises}
     </p>
@@ -35,9 +44,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content parts={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]}/>
       <Total ex={exercises1 + exercises2 + exercises3} />
     </div>
   )
