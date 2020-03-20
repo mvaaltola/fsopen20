d75cf379593e6868c1945753ff4884fc1452e026
Mikael Vaaltola <mvaaltola@gmail.com>
2020-03-20 13:38:15 +0200

    Complete 1.13

--- 1/anekdootit/src/index.js
+++ 1/anekdootit/src/index.js
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(
+    new Array(6+1).join('0').split('').map(parseFloat))
   const nextAnecdote = () => {
     setSelected(Math.round(Math.random()*(anecdotes.length-1)))
+  }
+  const increasePoints = () => {
+    const newPoints = [...points]
+    newPoints[selected] += 1
+    setPoints(newPoints)
   }
 
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
+      <p>{props.anecdotes[selected]}<br />
+      has {points[selected]} votes</p>
       <button onClick={nextAnecdote}>next anecdote</button>
+      <button onClick={increasePoints}>vote</button>
     </div>
   )
 }
@@ -23,6 +32,7 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
+
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
