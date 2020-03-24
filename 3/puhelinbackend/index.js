a53386b49335e05348cf379438b83259f6827c9c
Mikael Vaaltola <mvaaltola@gmail.com>
2020-03-24 18:49:47 +0200

    Complete 3.8

--- 3/puhelinbackend/index.js
+++ 3/puhelinbackend/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const token = morgan.token('postprinter', (req, res) => { return JSON.stringify(req.body) })
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postprinter'))
 
 let persons = [
     {
