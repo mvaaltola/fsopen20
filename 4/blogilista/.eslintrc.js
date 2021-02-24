6594a217e3bf9fc5d7b22b74ba859bc10ee37dd3
Mikael Vaaltola <mvaaltola@gmail.com>
2021-02-24 17:00:52 +0200

    Complete 4.1

---
+++ 4/blogilista/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'node': true,
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 2020,
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'never',
+    ],
+    'no-unused-vars': [
+      2,
+      {
+        'args': 'after-used',
+        'argsIgnorePattern': '^_'
+      }
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0,
+  },
+}
