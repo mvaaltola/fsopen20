17e1aa394846c51c19c490ddafc13addd11c8060
Mikael Vaaltola <mvaaltola@gmail.com>
2021-02-28 20:24:56 +0200

    Complete 4.18, 4.19

--- 4/blogilista/controllers/blogs.js
+++ 4/blogilista/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -9,18 +18,23 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog(request.body)
-  const user = await User.findById(request.body.userId)
   if (!user) {
     return response.status(400).json({ error: 'invalid/missing userId' })
   }
   blog.user = user._id
 
-  const result = await blog.save()
-  user.blogs = user.blogs.concat(result._id)
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
 
-  response.status(201).json(result.toJSON())
+  response.status(201).json(savedBlog.toJSON())
 })
 
 blogsRouter.delete('/:id', async(request, response) => {
